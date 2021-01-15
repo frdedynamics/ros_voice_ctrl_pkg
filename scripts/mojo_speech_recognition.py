3b9489d9f376872e63f94d3987151323120649f5
Johannes Møgster <johannes.mogster@mojotech.no>
2021-01-15 14:55:23 +0100

    closes #1, closes #4, closes #6
    no keyword though, and it`s the timeout that ensures stopping "graceful".

--- scripts/mojo_speech_recognition.py
+++ scripts/mojo_speech_recognition.py
@@ -8,38 +8,46 @@ from std_msgs.msg import String
 
 RECORDING = sr.Recognizer()
 
+
 def talker():
     """ docsctring, yo! """
     speech_recognition_pub = rospy.Publisher('speech_recognition', String, queue_size=1)
-    
+
     rospy.init_node('speech_recognizer', anonymous=True)
 
     rate = rospy.Rate(1)
-    
+    speech_recognized = ''
     while not rospy.is_shutdown():
         with sr.Microphone() as source:
+            speech_is_recognized = True
             RECORDING.adjust_for_ambient_noise(source)
             print("Please Say something:")
-            audio = RECORDING.listen(source)
-
             try:
+                audio = RECORDING.listen(source, timeout=2)
                 speech_recognized = RECORDING.recognize_google(audio)
-            except sr.UnknownValueError as e:
-                print("I didn`t get it..")
-        try:
-            print("You said: \n" + speech_recognized)
-        except Exception as e:
-            print(e)
 
-        msg = String()
-        msg.data = speech_recognized
-        speech_recognition_pub.publish(msg)
+            except sr.WaitTimeoutError as e:
+                speech_is_recognized = False
+                speech_recognized = ''
+                print("Timed out..")
+
+            except sr.UnknownValueError as e:
+                speech_is_recognized = False
+                print("I didn`t get it..")
+
+        if speech_is_recognized:
+            print("You said: \n" + speech_recognized)
+            msg = String()
+            msg.data = speech_recognized
+            speech_recognition_pub.publish(msg)
+
         rate.sleep()
+
 
 if __name__ == '__main__':
     MYARGV = rospy.myargv(argv=sys.argv)
     if len(MYARGV) > 1:
-        print("args not cared: ", URL)
+        print("args not cared.. ")
     try:
         talker()
     except rospy.ROSInterruptException:
